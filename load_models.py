840075103592044d8c8d4f61916edb858c242707
Jeremy Saslaw <saslaw.jeremy@gmail.com>
2020-03-22 23:10:56 -0400

    working get_gamedate function, stored 2018-19 season, gamedate, and players

--- load_models.py
+++ load_models.py
@@ -2,11 +2,20 @@
 
 from argparse import ArgumentParser
 from mongoengine import connect
+import datetime
+from time import sleep
 
 from nba_api.stats.static import players as nba_api_players
-from nba_api.stats.endpoints import leaguedashplayerstats as player_stats
+from nba_api.stats.endpoints import (
+    leaguedashplayerstats,
+    scoreboardv2
+)
 
-from models import Player
+from models import (
+    Player,
+    GameDate,
+    Season
+)
 
 
 def get_players(years):
@@ -22,7 +31,7 @@ def get_players(years):
 
         # Pull a list of all player ids from this year
         year_stats = (
-            player_stats.LeagueDashPlayerStats(season=year)
+            leaguedashplayerstats.LeagueDashPlayerStats(season=year)
             .get_data_frames()[0]
         )
         players_info = [player_info for player_info in
@@ -47,16 +56,16 @@ def get_players(years):
 
             # Else player doesn't yet exist in database
             else:
-                print("Adding {} to database".format(player_name))
                 player_entry = Player()
                 player_info = nba_api_players.find_player_by_id(player_id)
                 player_entry.name = player_name
                 player_entry.player_id = player_id
                 player_entry.years = [year]
                 player_entry.save()
+                print("Adding {} to database".format(player_name))
 
 
-def get_dates(years):
+def get_gamedates(years):
     """
     Load the dates and games played on them for the given years supplied
 
@@ -67,45 +76,73 @@ def get_dates(years):
     # For each specified year, look at the dates and games played on them
     for year in years:
 
-        # Pull a list of all player ids from this year
-        year_stats = (
-            player_stats.LeagueDashPlayerStats(season=year)
-            .get_data_frames()[0]
-        )
-        players_info = [player_info for player_info in
-                        zip(year_stats['PLAYER_ID'], year_stats['PLAYER_NAME'])
-                       ]
+        season_entry = Season()
+        season_entry.year = year
 
-        # If player already exists in database, update their years accordingly,
-        # else, add player to database
-        for player_id, player_name in players_info:
+        # Get the first day of October as the first possible default date
+        first_date = '{}-10-01'.format(year[:4])
+        day_delta = datetime.timedelta(days=1)
 
-            player_entry = Player.objects(player_id=player_id)
+        # Iterate until finding first day of regular season
+        while True:
+            sleep(1) # Don't overload querying
+            gameday = scoreboardv2.ScoreboardV2(game_date=first_date)
+            print("Looking at {} for first day of season".format(first_date))
+            game_ids = gameday.available.get_data_frame()['GAME_ID']
 
-            # Player already exists in database
-            if player_entry:
-                player_entry = player_entry[0]
-                # If year isn't already stored for this player
-                if year not in player_entry.years:
-                    print("Adding {} to {} in the database"
-                          .format(year, player_name))
-                    player_entry.years.append(year)
-                    player_entry.save()
-
-            # Else player doesn't yet exist in database
+            # If there were games this day, and it is regular season
+            if len(game_ids)>0 and game_ids[0][2]=='2':
+                season_entry.first_date = first_date
+                break
             else:
-                print("Adding {} to database".format(player_name))
-                player_entry = Player()
-                player_info = nba_api_players.find_player_by_id(player_id)
-                player_entry.name = player_name
-                player_entry.player_id = player_id
-                player_entry.years = [year]
-                player_entry.save()
+                first_date = (
+                    datetime.date.fromisoformat(first_date) + day_delta
+                    ).isoformat()
+
+        # Begin loading into mongo the game dates
+        date = first_date
+        while True:
+
+            gamedate_entry = GameDate.objects(date=date)
+
+            # Game date already exists in database
+            if gamedate_entry:
+                print('{} is already in the database'.format(date))
+            # Else game date is not already in database
+            else:
+                sleep(1) # Don't overload querying
+                gameday = scoreboardv2.ScoreboardV2(game_date=date)
+                game_ids = (
+                    gameday.available.get_data_frame()['GAME_ID'].to_list()
+                )
+
+                # If all star game, skip
+                if len(game_ids)>0 and game_ids[0][2] == '3':
+                    game_ids = []
+                # If playoff game, stop and mark previous date as last day
+                if len(game_ids)>0 and game_ids[0][2] == '4':
+                    last_date = (
+                        datetime.date.fromisoformat(date) - day_delta
+                        ).isoformat()
+                    season_entry.last_date = last_date
+                    season_entry.save()
+                    break
+
+                # Create gameday entry for this day
+                gamedate_entry = GameDate()
+                gamedate_entry.date = date
+                gamedate_entry.year = year
+                gamedate_entry.games = game_ids
+                gamedate_entry.save()
+                print('Adding {} to database with {} games played on '
+                      'this day'.format(date, len(game_ids)))
+
+            date = (datetime.date.fromisoformat(date) + day_delta).isoformat()
 
 if __name__ == '__main__':
 
     parser = ArgumentParser()
-    parser.add_argument('--years', default=['2019-20'], nargs='*',
+    parser.add_argument('--years', default=['2018-19'], nargs='*',
                         help='Years to acquire player data for. Please use the'
                              ' format xxxx-xx, e.g. 2019-20.'
                        )
@@ -113,7 +150,7 @@ if __name__ == '__main__':
                         help='Get player data to store under '
                              'Player collection.'
                        )
-    parser.add_argument('--dates', action='store_true',
+    parser.add_argument('--gamedates', action='store_true',
                         help='Get dates and games played on those dates.'
                        )
     args = parser.parse_args()
@@ -124,5 +161,5 @@ if __name__ == '__main__':
     # Call the requested data acquiring functions
     if args.players:
         get_players(args.years)
-    if args.dates:
-        get_dates(args.years)
+    if args.gamedates:
+        get_gamedates(args.years)
--- models.py
+++ models.py
@@ -12,3 +12,18 @@ class Player(Document):
     player_id = IntField()
     name = StringField()
     years = ListField()
+
+
+class GameDate(Document):
+
+    """  GameDate representation. """
+    year = StringField()
+    date = StringField()
+    games = ListField()
+
+class Season(Document):
+
+    """ Season representation. """
+    year = StringField()
+    first_date = StringField()
+    last_date = StringField()
