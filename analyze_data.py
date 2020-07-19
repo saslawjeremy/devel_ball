3835c3e93a357f8c7af6e4e49e2d43e556ea6386
Jeremy Saslaw <saslaw.jeremy@gmail.com>
2020-07-19 15:26:23 -0400

    working pipeline thus far

--- analyze_data.py
+++ analyze_data.py
@@ -12,24 +12,51 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OrdinalEncoder
 
+
+class NegativeValueRemover(BaseEstimator, TransformerMixin):
+    """
+    Custom transformer to remove negative values from the features that are now allowed
+    to have them.
+    """
+
+    def __init__(self, exclude_features=
+            ['PLUS_MINUSpg', 'PLUS_MINUSpm', 'PLUS_MINUSpp', 'PER', 'GAME_SCORE', 'PIE',
+             'PLUS_MINUStm', 'PLUS_MINUSvsTm']):
+        self.exclude_features = exclude_features
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+
+        for cat in X:
+            if cat not in self.exclude_features:
+                X.loc[X[X[cat]<0.0][cat].index, cat] = 0
+        return X
+
+
 class OutlierRemover(BaseEstimator, TransformerMixin):
     """
     Custom transformer to remove outliers from the dataset.
     """
 
     def __init__(self, std_devs_for_outlier=4):
-        self._std_devs_for_outlier = std_devs_for_outlier
-        self._means = {}
-        self._std_devs = {}
+        self.std_devs_for_outlier = std_devs_for_outlier
 
     def fit(self, X, y=None):
+        self._means = {}
+        self._std_devs = {}
         for cat in list(X):
             self._means[cat] = X[cat].mean()
             self._std_devs[cat] = X[cat].std()
         return self
 
     def transform(self, X):
-        import IPython; IPython.embed()
+        for cat in list(X):
+            pos_limit = self._means[cat] + self.std_devs_for_outlier * self._std_devs[cat]
+            neg_limit = self._means[cat] - self.std_devs_for_outlier * self._std_devs[cat]
+            X.loc[X[X[cat] > pos_limit].index, cat] = pos_limit
+            X.loc[X[X[cat] < neg_limit].index, cat] = neg_limit
         return X
 
 
@@ -50,9 +77,9 @@ if __name__ == '__main__':
     data_Y = data[predictions].copy()
 
     num_pipeline = Pipeline([
+        ('negative_value_remover', NegativeValueRemover()),
         ('outlier_remover', OutlierRemover()),
     ])
-
     num_attribs = list(data_X)
     num_attribs.remove('HOME')
     cat_attribs = ['HOME']
@@ -66,13 +93,3 @@ if __name__ == '__main__':
     data_X_prepared = pd.DataFrame(data_X_prepared_np, data_X.index, data_X.columns)
 
     print(data_X_prepared)
-
-    """
-    Player traditional stats per game/min/poss + rates:
-      all but PLUS_MINUS
-        - max (if value > std_dev*4, value = std_dev*4)
-        - min (if value < 0, value = 0)
-      PLUS_MINUS
-        - max and min (std_dev*4)
-    """
-
