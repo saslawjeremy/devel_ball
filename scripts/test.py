208ca84845884bbbe3dedd5b443b186de8cc0849
Jeremy Saslaw <saslaw.jeremy@gmail.com>
2023-01-07 00:12:36 -0500

    successfully mongo-ing

--- scripts/test.py
+++ scripts/test.py
@@ -4,6 +4,32 @@ from selenium import webdriver
 from time import sleep
 import pandas
 import attr
+from mongoengine import connect
+from mongoengine import (
+    Document,
+    StringField,
+    FloatField,
+    IntField,
+    DateTimeField,
+)
+from datetime import (
+    datetime,
+    timedelta,
+)
+
+
+class Prop(Document):
+    date_created = DateTimeField(default=datetime.utcnow)
+    date_updated = DateTimeField(default=datetime.utcnow)
+    team1 = StringField()
+    team2 = StringField()
+    gametime = DateTimeField()
+    player = StringField()
+    category = StringField()
+    over = FloatField()
+    over_odds = IntField()
+    under = FloatField()
+    under_odds = IntField()
 
 
 @attr.s
@@ -88,7 +114,20 @@ CATEGORIES = [
 ]
 
 
-def get_data(driver, url):
+def get_gametime(todayOrTomorrow, time):
+    date = datetime.now() if todayOrTomorrow == 'TODAY' else datetime.now() + timedelta(days=1)
+    amOrPm = 'AM' if 'AM' in time else 'PM'
+    timeNumber = time.split(amOrPm)[0]
+    hour = int(timeNumber.split(':')[0])
+    if amOrPm == 'PM':
+        hour += 12
+    mins = int(timeNumber.split(':')[1])
+    gametime = datetime(date.year, date.month, date.day, hour, mins)
+    # TODO (JS): figure out timezones
+    return gametime
+
+
+def get_data(driver, url, category):
 
     # Load page and sleep so it finishes loading
     driver.get(url)
@@ -98,8 +137,8 @@ def get_data(driver, url):
     playerTables = pandas.read_html(driver.page_source)
     # Clean them
     for playerTable in playerTables:
-        playerTable.insert(2, "OVER odds", None)
-        playerTable.insert(4, "UNDER odds", None)
+        playerTable.insert(2, "OVER_odds", None)
+        playerTable.insert(4, "UNDER_odds", None)
         for _, player in playerTable.iterrows():
             for bet in ('OVER', 'UNDER'):
                 # - is represented by chr(8722)
@@ -114,7 +153,7 @@ def get_data(driver, url):
                 else:
                     raise Exception("Unexpected state, investigate!")
                 player[bet] = val
-                player['{} odds'.format(bet)] = odds
+                player['{}_odds'.format(bet)] = odds
 
     # Have to be a bit more creative to get the game names / times (if they're today/tomorrow)
     allGames = driver.find_elements(By.CLASS_NAME, 'sportsbook-event-accordion__wrapper')
@@ -132,31 +171,65 @@ def get_data(driver, url):
         if 'TODAY' in part2List:
             index = part2List.index('TODAY')
             team2 = ' '.join(part2List[:index])
-            time = ' '.join(part2List[index:])
+            gametime = get_gametime(part2List[index:][0], part2List[index:][1])
         elif 'TOMORROW' in part2List:
             index = part2List.index('TOMORROW')
             team2 = ' '.join(part2List[:index])
-            time = ' '.join(part2List[index:])
+            gametime = get_gametime(part2List[index:][0], part2List[index:][1])
         else:
             team2 = ' '.join(part2List)
-            time = None
+            gametime = 'LIVE'
 
-        gameDatas.append([team1, team2, time])
+        gameDatas.append([team1, team2, gametime])
 
     print("PRINTING DATA:   ")
     for gameData, playerTable in zip(gameDatas, playerTables):
-        print("{} vs. {}   ({})".format(gameData[0], gameData[1], gameData[2] if gameData[2] else "LIVE"))
+        print("{} vs. {}   ({})".format(gameData[0], gameData[1], gameData[2]))
         print(playerTable)
         print("\n")
+
+    for gameData, playerTable in zip(gameDatas, playerTables):
+        for _, player in playerTable.iterrows():
+            # Don't save off live games
+            if gameData[2] == 'LIVE':
+                continue
+            # See if existing doc for this prop exists
+            doc = Prop.objects(
+                team1=gameData[0], team2=gameData[1], gametime=gameData[2], category=category, player=player.PLAYER
+            ).first()
+            if doc is not None:
+                doc.over = player.OVER
+                doc.over_odds = player.OVER_odds
+                doc.under = player.UNDER
+                doc.under_odds = player.UNDER_odds
+                doc.date_updated = datetime.utcnow()
+            else:
+                doc = Prop(
+                    team1=gameData[0],
+                    team2=gameData[1],
+                    gametime=gameData[2],
+                    player=player.PLAYER,
+                    category=category,
+                    over=player.OVER,
+                    over_odds=player.OVER_odds,
+                    under=player.UNDER,
+                    under_odds=player.UNDER_odds
+                )
+            doc.save()
+
 
 def get_all_data(driver):
     for category in CATEGORIES:
         url = 'https://sportsbook.draftkings.com/leagues/basketball/nba?category={}&subcategory={}'.format(category.category, category.subcategory)
         print("\nGetting data for: {}".format(category.name))
         print("Scraping: {}".format(url))
-        get_data(driver, url)
+        get_data(driver, url, category.name)
 
 def main():
+    # Connect to the local mongo client and devel_ball database
+    mongo_client = connect('DraftKings_Props')
+
+    # Scrape the data
     options = Options()
     options.add_argument("window-size=2000,1200")
     try:
